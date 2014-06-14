c14aa36b8d4e25dcbd458db22d0621005ce1808f
Jeff Richardson <jeffreywebdevelopment@gmail.com>
2014-06-14 12:41:44 -0500

    hardcoded profile view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 LoveRealm::Application.routes.draw do
   get "users/show"
   resources :users
+
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
 
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
