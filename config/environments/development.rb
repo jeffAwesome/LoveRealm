8c66f629e0bc5ab95e2f2ef60a3a880b5775cc81
Jeff Richardson <jeffreywebdevelopment@gmail.com>
2014-06-14 12:28:05 -0500

    conflict fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,12 @@ LoveRealm::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+    # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -27,4 +31,7 @@ LoveRealm::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 LoveRealm::Application.routes.draw do
   get "users/show"
   resources :users
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+
+  match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   root 'home#index'
 
   %w( 404 422 500 ).each do |code|
